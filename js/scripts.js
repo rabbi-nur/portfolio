3b69158809d2e242d14bd1b42f5acb54a3fe4409
Rabbinur <140503888+rabbi-nur@users.noreply.github.com>
2023-08-23 16:50:47 +0600

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,74 @@
+        // icon
+        let menuIcon = document.querySelector('#menu-icon');
+        let navbar   = document.querySelector('.navbar');
+
+        menuIcon.onclick  = () => {
+            menuIcon.classList.toggle('bx-x');
+            navbar.classList.toggle('active');
+        };
+        
+        
+        // scroll section active link
+
+        let section  = document.querySelectorAll('section');
+        let navlinks = document.querySelectorAll('header nav a');
+
+
+        window.oncancel = () => {
+            section.forEach(sec => {
+                let top = window.scrollY;
+                let offset = sec.offsetTop -150;
+                let height = sec.offsetHeight;
+                let id = sec.getAttribute('id');
+
+                if( top >= offset && top < offset + height) {
+                    navlinks.forEach(link => { 
+                        links.classlist.remove('active');
+                        document.querySelector('header nav a[herf*=' + id +']').classList.add('active');
+                    }); 
+
+                };
+
+            });
+
+            let header = document.querySelector('header');
+
+            header.classList.toggle('sticky', window.scrollY > 100);
+
+    //  Remove toggle icone and navbar 
+        menuIcon.classList.remove('bx-x');
+        navbar.classList.remove('active');
+
+  };
+
+        ScrollReveal({
+            //   reset: true  ,
+            distance:'80px',
+            duration:2000,
+            delay:200,
+
+        });
+        ScrollReveal().reveal('.home-content, .heading',{ origin: 'top'});
+        ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form',{ origin: 'bottom'});
+        ScrollReveal().reveal('.home-content h1, .about-img',{ origin: 'left'});
+        ScrollReveal().reveal('.home-content p, .about-contact',{ origin: 'right'});
+
+        // Type js Full Stack Developer
+
+       const typed = new Typed ('.multiple-text', {
+            Strings: ['Full Stack Developer', 'Frontend Developer' , 'Backend Developer'],
+            typeSpeed:100,
+            backSpeed:100,
+            backDelay:1000,
+            loop:true
+
+        });
+
+        // var typed = new Typed(".mov", {
+        //     strings: [,  "Ganesh","Content Writer","Web Developer"  ,"Web Developer"] ,
+        //     typeSpeed: 100,
+        //     backSpeed: 100,
+        //     loop: true,
+        //     });
+
+        
